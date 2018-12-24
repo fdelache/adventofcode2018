2620c2b8bfdbafb7d5dd38e1199273130b3c6843
Franck Delache <43553065+fdelache@users.noreply.github.com>
2018-12-24 11:30:25 -0500

    Completion of puzzle_15
    This one was verrrrrry long!

--- puzzle_15.rb
+++ puzzle_15.rb
@@ -39,6 +39,10 @@ class Cell
   def unit?
     false
   end
+
+  def render
+    type
+  end
 end
 
 class Unit < Cell
@@ -52,8 +56,16 @@ class Unit < Cell
     @hit_points = 200
   end
 
+  def attack_points
+    type == 'G' ? 3 : map.elves_attack
+  end
+
   def hit_points
     @hit_points < 0 ? 0 : @hit_points
+  end
+
+  def render
+    alive? ? super : '.'
   end
 
   def free?
@@ -89,7 +101,7 @@ class Unit < Cell
     map.unit_surround_position(position)
       .any? do |position|
         map.fetch_cell(position)&.type != self.type
-      end
+    end
   end
 
   def compute_distances
@@ -135,21 +147,26 @@ class Unit < Cell
 
     return nil if target_positions.empty?
 
-    binding.pry
-    closest_positions = target_positions.mins_by do |position|
+    # binding.pry
+    closest_positions = target_positions.reject do |position|
+      x, y = position
+      distances[y][x].nil?
+    end.mins_by do |position|
       x, y = position
       distances[y][x]
     end
 
     closest_position = closest_positions.mins_by { |position| position[1] }
-                    .mins_by { |position| position[0] }
-                    .first
+      .mins_by { |position| position[0] }
+      .first
+
+    return nil if closest_position.nil?
 
     paths = compute_paths_to(closest_position, distances)
 
     paths.mins_by { |path| path.first[1] }
-         .mins_by { |path| path.first[0] }
-         .first
+      .mins_by { |path| path.first[0] }
+      .first
   end
 
   def compute_paths_to(position, distances)
@@ -159,13 +176,16 @@ class Unit < Cell
     return [[position]] if current_distance == 1
 
     free_cells = map.free_surround_positions(position)
-    parent_cells = free_cells.mins_by do |position|
+    parent_cells = free_cells.reject do |position|
+      x, y = position
+      distances[y][x].nil?
+    end.mins_by do |position|
       x, y = position
       distances[y][x]
     end
 
     parent_cells.flat_map { |cell| compute_paths_to(cell, distances) }
-                .map { |path| path.push(position) }
+      .map { |path| path.push(position) }
   end
 
   def target_units
@@ -176,13 +196,13 @@ class Unit < Cell
     return unless next_to_target?
 
     target = map.unit_surround_position(position)
-       .map { |position| map.fetch_cell(position) }
-       .select { |unit| unit.type != self.type }
-       .mins_by { |unit| unit.hit_points }
-       .sort
-       .first
+      .map { |position| map.fetch_cell(position) }
+      .select { |unit| unit.type != self.type }
+      .mins_by { |unit| unit.hit_points }
+      .sort
+      .first
 
-    target.hit_points -= 3
+    target.hit_points -= attack_points
   end
 
   def distance(other_position)
@@ -205,7 +225,8 @@ end
 class Map
   attr_reader :map
   attr_reader :units
-  attr_reader :rounds
+  attr_reader :rounds, :input
+  attr_accessor :elves_attack
 
   FREE_CELL = Cell.new('.')
   WALL_CELL = Cell.new('#')
@@ -215,10 +236,12 @@ class Map
   end
 
   def initialize(input)
+    @input = input
     @rounds = 0
     @map = Array.new(input.length) { Array.new }
     @units = []
-    
+    @elves_attack = 3
+
     input.each_with_index do |line, y|
       line.chomp.each_char.each_with_index do |c, x|
         cell = nil
@@ -236,11 +259,14 @@ class Map
   end
 
   def round
-    units.sort.each do |unit|
+    sorted_units = units.sort.select { |unit| unit.alive? }
+    sorted_units.each do |unit|
       unit.round if unit.alive?
-    end
 
-    @rounds += 1
+      @rounds += 1 if sorted_units.last == unit
+
+      return if no_targets_remaining
+    end
   end
 
   def part1
@@ -252,7 +278,42 @@ class Map
       break if no_targets_remaining
     end
 
+    render
+    render_units
     puts "Part1: #{rounds * units_remaining_points}"
+  end
+
+  def round_til_first_elf_death_or_win
+    loop do
+      round
+
+      return false if one_elf_dies
+      break if no_targets_remaining
+    end
+
+    true
+  end
+
+  def part2
+    trial = nil
+
+    (4..Float::INFINITY).each do |elf_attack|
+      trial = Map.new(input)
+      trial.elves_attack = elf_attack
+
+      puts "Trying with elf attack: #{elf_attack}"
+
+      break if trial.round_til_first_elf_death_or_win
+    end
+
+    trial.render
+    trial.render_units
+    puts "Part2: #{trial.rounds * trial.units_remaining_points}"
+  end
+
+  def one_elf_dies
+    units.select { |unit| unit.type == 'E' }
+      .any? { |elf| !elf.alive? }
   end
 
   def units_remaining_points
@@ -281,12 +342,13 @@ class Map
 
   def free_surround_positions(position)
     [[-1, 0], [0, 1], [1, 0], [0, -1]].map { |delta| [position[0] + delta[0], position[1] + delta[1]] }
-                                      .select { |position| free_cell(position) }
+      .select { |position| free_cell(position) }
   end
 
   def unit_surround_position(position)
     [[-1, 0], [0, 1], [1, 0], [0, -1]].map { |delta| [position[0] + delta[0], position[1] + delta[1]] }
-                                      .select { |position| unit?(position) }
+      .select { |position| unit?(position) }
+      .select { |position| fetch_cell(position).alive? }
   end
 
   def move(unit, new_position)
@@ -304,14 +366,19 @@ class Map
     puts "Map at round #{rounds}:"
     map.each do |row|
       row.each do |cell|
-        print "#{cell.type}"
+        print "#{cell.render}"
       end
       puts ''
     end
   end
+
+  def render_units
+    units.select { |unit| unit.alive? }
+      .each { |unit| puts "#{unit.type}(#{unit.hit_points})"}
+  end
 end
 
-SAMPLE1=<<-EOS.split(/\n/)
+SAMPLE0 = <<-EOS.split(/\n/)
 #######
 #.G...#
 #...EG#
@@ -321,5 +388,59 @@ SAMPLE1=<<-EOS.split(/\n/)
 #######
 EOS
 
+SAMPLE1 = <<-EOS.split(/\n/)
+#######
+#G..#E#
+#E#E.E#
+#G.##.#
+#...#E#
+#...E.#
+#######
+EOS
+
+SAMPLE2 = <<-EOS.split(/\n/)
+#######
+#E..EG#
+#.#G.E#
+#E.##E#
+#G..#.#
+#..E#.#
+#######
+EOS
+
+SAMPLE3 = <<-EOS.split(/\n/)
+#######
+#E.G#.#
+#.#G..#
+#G.#.G#
+#G..#.#
+#...E.#
+#######
+EOS
+
+SAMPLE4 = <<-EOS.split(/\n/)
+#######
+#.E...#
+#.#..G#
+#.###.#
+#E#G#G#
+#...#G#
+#######
+EOS
+
+SAMPLE5 = <<-EOS.split(/\n/)
+#########
+#G......#
+#.E.#...#
+#..##..G#
+#...##..#
+#...#...#
+#.G...G.#
+#.....G.#
+#########
+EOS
+
+data = File.readlines('./data/day_15_input')
+
 # binding.pry
-Map.new(SAMPLE1).part1
+Map.new(data).part2
